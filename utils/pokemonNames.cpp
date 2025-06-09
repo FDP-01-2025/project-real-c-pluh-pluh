49f789962663d61f3c646ae51f85e1a96906efb8
yorchhh10 <00072325@uca.edu.sv>
2025-06-09 14:57:41 -0300

    add: Jorge
    Seleccion de el personaje a ocupar

---
+++ utils/pokemonNames.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+    string Pokemon[10] = {"Squirtle", "Lapras", "Charmander", "Flareon", "Bulbasaur", 
+                          "Oddish", "Pikachu", "Magnemite", "Onix", "Geodude"};
+   bool IsOk = false; //Ocupe IsOk para que se cree un bucle y elija una opcion si o si
+    do {
+        // Mostrar todos los Pokémon
+        cout << "Lista de Pokémon:\n";
+        for (int i = 0; i < 10; ++i) {
+            cout << i + 1 << ". " << Pokemon[i] << endl;
+        }
+    
+        // Pedir al usuario que elija uno
+       /
+    }
