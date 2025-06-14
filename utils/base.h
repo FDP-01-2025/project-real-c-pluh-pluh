6eb1e2b45bff2882ebf547f3aaa1caf73bf560a6
almightytgc <00135025@uca.edu.sv>
2025-06-13 18:56:41 -0600

    add: pokemon data setter

--- utils/base.h
+++ utils/base.h
@@ -2,6 +2,32 @@
 #define COMMON_H
 
 #include <iostream>
+#include <limits> // para numeric_limits
+#include <random> //para la función de número random
+#include <cstdlib>  // para system()
+
 using namespace std;
 
+void cleanInput() {
+    cin.clear(); // Se limpia lo que se guardó en el cin
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // descarta todos los "residuos"
+}
+
+int getRandomNumber(int minNumber, int maxNumber) {
+    //objeto rd de la clase random_device de la librería
+    random_device rd;
+    //generador, quién sabe por qué ese nombre gen es un objeto de la clase mt19937
+    mt19937 gen(rd());
+
+    //hacemos la distribucion o definimos el rango
+    uniform_int_distribution<> distrib(minNumber, maxNumber);
+
+    int randomNumber = distrib(gen);
+
+    return randomNumber;
+}
+
+void cleanScreen() {
+    system("cls");
+}
 #endif
