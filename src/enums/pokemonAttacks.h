57de50e756ba123dd9531fbeacb257bdb07b8aee
dgonzag <00072825@uca.edu.sv>
2025-06-17 11:59:31 -0600

    Actualizacion pokemonAttacks.h
    Actualizacion de tipos de Ataques de los Pokemons

---
+++ src/enums/pokemonAttacks.h
@@ -0,0 +1,10 @@
+#ifndef POKEMON_ATTACKS_H // Chequear si no se ha definido ya el "header".
+#define POKEMON_ATTACKS_H // Definir el "header" si no se ha hecho ya.
+
+enum PokemonAttackType { // Enumeración para los tipos de ataques de Pokémon
+    NORMAL_ATTACK = 1,  // Ataque Normal
+    SPECIAL_ATTACK = 2  // Ataque Especial
+};
+
+#endif // POKEMON_ATTACKS_H
+// Fin del "header" de tipos de ataques de Pokémon.
