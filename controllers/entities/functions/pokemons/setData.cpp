08b7d4c411c5e809384a2d0fc59f4c245be93f87
eobardThawnetgc <00135025@uca.edu.sv>
2025-06-19 20:24:34 -0600

    Refactor: set pokemon data function

--- controllers/entities/functions/pokemons/setData.cpp
+++ controllers/entities/functions/pokemons/setData.cpp
@@ -1,76 +1,77 @@
 #include "../../../../utils/base.h"
 #include "../../../../src/entities/structs.h"
 
+int setPokemonType(int selectedPokemon);
+int setPokemonHealth(int pokemonType);
+int setWeaknessType(int pokemonType);
+int setDamage(int pokemonType);
+
+Pokemon setData() {
+    Pokemon character;
+
+    string pokemonName;
+    int selectedPokemon;
+
+    cout << "\n Dinos tu nombre entrenador: ";
+    character.coach = askForString();
+    cout << "\n " << character.coach << " ahora selecciona a tu pokemon: (Digita el número que le corresponda al pokemon que desees elegir): ";
+    selectedPokemon = askForInteger();
+    //character.name = pokemonName, falta su lógica
+    character.type = setPokemonType(selectedPokemon);
+    character.baseHealth = setPokemonHealth(character.type);
+    character.typeOfWeakness = setWeaknessType(character.type);
+    character.damage = setDamage(character.type);
+    
+    return character;
+}
+
 int setPokemonType(int selectedPokemon) {
-    int pokemonType;
+    if (selectedPokemon >= 0 && selectedPokemon <= 1) return 1;
+    else if (selectedPokemon >= 2 && selectedPokemon <= 3) return 1;
+    else if (selectedPokemon >= 4 && selectedPokemon <= 5) return 1;
+    else if (selectedPokemon >= 6 && selectedPokemon <= 7) return 1;
+    else if (selectedPokemon >= 8 && selectedPokemon <= 9) return 1;
 
-    if (selectedPokemon >= 0 && selectedPokemon <= 1) {
-
-    } else if (selectedPokemon >= 2 && selectedPokemon <= 3) {
-
-    } else if (selectedPokemon >= 4 && selectedPokemon <= 5) {
-
-    } else if (selectedPokemon >= 6 && selectedPokemon <= 7) {
-
-    } else if (selectedPokemon >= 8 && selectedPokemon <= 9) {
-
-    }
-
-    return pokemonType;
+    // Si no entra en ningún rango válido, retorno -1 (error)
+    cout << "\n Pokémon seleccionado no válido.";
+    return -1;
 }
 
 int setPokemonHealth(int pokemonType) {
-    int pokemonHealth;
-
     switch(pokemonType) {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
+        case 1: return 100;
+        case 2: return 100;
+        case 3: return 100;
+        case 4: return 100;
+        case 5: return 100;
         default:
-            cout << "\n El número ingresado no es válido";
-            break;            
+            cout << "\n Tipo de Pokémon no válido para salud.";
+            return -1;
     }
-
-    return pokemonHealth;
 }
 
 int setWeaknessType(int pokemonType) {
-    int weaknessType;
-
     switch(pokemonType) {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
+        case 1: return 1;
+        case 2: return 1;
+        case 3: return 1;
+        case 4: return 1;
+        case 5: return 1;
         default:
-            cout << "\n El número ingresado no es válido";
-            break;            
+            cout << "\n Tipo de Pokémon no válido para debilidad.";
+            return -1;
     }
-
-    return weaknessType;
 }
 
-void setData(Pokemon pokemon) {
-    string coachName, pokemonName;
-    int selectedPokemon, pokemonType, pokemonHealth, pokemonWeaknessType;
-
-    cout << "\n Dinos tu nombre entrenador: ";
-    coachName = askForString();
-    cout << "\n " << coachName << " ahora selecciona a tu pokemon: (Digita el número que le corresponda al pokemon que desees elegir): ";
-    pokemonName = askForInteger();
-
-    freezeInput();
+int setDamage(int pokemonType) {
+    switch(pokemonType) {
+        case 1: return 1;
+        case 2: return 1;
+        case 3: return 1;
+        case 4: return 1;
+        case 5: return 1;
+        default:
+            cout << "\n Tipo de Pokémon no válido para daño.";
+            return -1;
+    }
 }
