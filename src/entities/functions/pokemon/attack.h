e366d0741be88bb9b2fb4cb4ea9d9be16b2aff6d
dgonzag <00072825@uca.edu.sv>
2025-06-29 11:03:34 -0600

    Actualizacion en attack.h
    agregar & en las variables

--- src/entities/functions/pokemon/attack.h
+++ src/entities/functions/pokemon/attack.h
@@ -5,8 +5,8 @@
 #include "../../structs.h" // Incluir el "Header" de estructuras para las definiciones de Pokémon.
 #include "../../enums/pokemonAttacks.h" // Incluir el "Header" de enumeraciones para los tipos de ataques de Pokémon.
 
-void attackTurn(Pokemon playerOne, Pokemon playerTwo); // La función de ataque que toma dos Usuarios como parámetros.
-void performAttack(Pokemon attacker, Pokemon defender, int attackType); // La función que realiza el ataque, tomando el atacante, el defensor y el tipo de ataque como parámetros.
+void attackTurn(Pokemon &playerOne, Pokemon &playerTwo); // La función de ataque que toma dos Usuarios como parámetros.
+void performAttack(Pokemon &attacker, Pokemon &defender, int attackType); // La función que realiza el ataque, tomando el atacante, el defensor y el tipo de ataque como parámetros.
 
 
 #endif // ATTACK_H
