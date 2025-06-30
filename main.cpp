26984a9aba12eb0aec9db70a9d44d952e64e7f58
Pablo Antonio Brito Elías <00179025@uca.edu.sv>
2025-06-30 05:59:40 +0000

    testmenu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "./utils/base.h"
-#include "./src/game/game.h"
+#include <iostream>
 #include <windows.h>
+#include "./src/game/selectGameMode.h"
+#include "./utils/base.h"
+
+using namespace std;
 
 int main() {
-    //set utf8 system
+    
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    game();
+
+    clearScreen();     // Clear the console
+    showMainMenu();    // Display the menu
+
+    int mode = selectGameMode();  // Choose 1P or 2P
+    int rounds = selectRounds();  // Choose 3 or 5
+
+    cout << "\nYou selected mode: " << (mode == 1 ? "Single Player" : "Two Player") << endl;
+    cout << "Number of rounds: " << rounds << endl;
+
+    cout << "\n[ Game would start here... ]\n";
+
     return 0;
 }
