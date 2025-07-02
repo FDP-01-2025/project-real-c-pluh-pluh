c591a0d683e3249d473de7eac04eb50102134da4
Pablo Antonio Brito Elías <00179025@uca.edu.sv>
2025-07-02 03:36:27 +0000

    showMatchWinner

--- controllers/selectGameMode.cpp
+++ controllers/selectGameMode.cpp
@@ -58,5 +58,20 @@ int selectRounds() {
     } while (rounds != 3 && rounds != 5);
 
     return rounds;
+    // Determines and displays the match winner
+void showMatchWinner(int player1Wins, int player2Wins) {
+    cout << "\n============== MATCH RESULT ==============" << endl;
+
+    if (player1Wins > player2Wins) {
+        cout << "🎉 Player 1 wins the match!" << endl;
+    } else if (player2Wins > player1Wins) {
+        cout << "🎉 Player 2 wins the match!" << endl;
+    } else {
+        cout << "🤝 It's a draw!" << endl;
+    }
+
+    cout << "===========================================" << endl;
+}
+
 }
 
