199336c6c715baf103e090b6f5c8147e3e5296ee
Pablo Antonio Brito Elías <00179025@uca.edu.sv>
2025-07-06 18:11:51 +0000

    Cambio de menu

--- controllers/selectGameMode.cpp
+++ controllers/selectGameMode.cpp
@@ -1,27 +1,28 @@
-#include "./selectGameMode.h"
-#include "../utils/base.h"
+#include <iostream>
+#include <limits>
+#include "../src/game/selectGameMode.h"
 
 using namespace std;
 
 // Displays the main menu 
 void showMainMenu() 
 {
-    cleanScreen(); // Clear the console
-    cout << R"(  __  __           _             _          _                        
-                    |  \/  | ___   __| | ___     __| | ___    (_)_   _  ___  __ _  ___  
-                    | |\/| |/ _ \ / _` |/ _ \   / _` |/ _ \   | | | | |/ _ \/ _` |/ _ \ 
-                    | |  | | (_) | (_| | (_) | | (_| |  __/   | | |_| |  __/ (_| | (_) |
-                    |_|  |_|\___/ \__,_|\___/   \__,_|\___|  _/ |\__,_|\___|\__, |\___/ 
-                                                           |__/            |___/           
+    cout << R"(  -----
+
+    ____                          __  __           _       
+    / ___| __ _ _ __ ___   ___   |  \/  | ___   __| | ___  
+    | |  _ / _` | '_ ` _ \ / _ \ | |\/| |/ _ \ / _` |/ _ \
+    | |_| | (_| | | | | | |  __/ | |  | | (_) | (_| |  __/
+    \____|\__,_|_|_| |_| |_|\___||_|  |_|\___/ \__,_|\___|
+
+    -----
     )" << endl;
 
 
-    cout << "╔══════════════════════════════════════════════╗" << endl;
-    cout << "║               MENU PRINCIPAL                 ║" << endl;
-    cout << "╠══════════════════════════════════════════════╣" << endl;
-    cout << "║  1. Un jugador                               ║" << endl;
-    cout << "║  2. Dos jugadores                            ║" << endl;
-    cout << "╚══════════════════════════════════════════════╝" << endl;
+    cout << "=============== MAIN MENU ===============" << endl;
+    cout << "1. Single Player Mode" << endl;
+    cout << "2. Two Player Mode" << endl;
+    cout << "=========================================" << endl;
 }
 // Prompts the user to select a game mode (1 or 2 players)
 int selectGameMode() {
@@ -57,7 +58,6 @@ int selectRounds() {
     } while (rounds != 3 && rounds != 5);
 
     return rounds;
-}
     // Determines and displays the match winner
 void showMatchWinner(int player1Wins, int player2Wins) {
     cout << "\n============== MATCH RESULT ==============" << endl;
@@ -73,5 +73,5 @@ void showMatchWinner(int player1Wins, int player2Wins) {
     cout << "===========================================" << endl;
 }
 
-
+}
 
